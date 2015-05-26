ce5e1b068b0b0234f732b74ec2247ec2cb53db8e
ypw <915505626@qq.com>
2015-05-26 14:00:17 +0800

    master

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("cmd.exe");
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            button1.Location = new Point(r.Next(0, this.Width - button1.Width), r.Next(0, this.Height  - button1.Height ));
+        }
+    }
+}
